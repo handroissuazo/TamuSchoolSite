1a5b87291a3b2dfb0a50ed6248c3490dcba31b28
alejandrojsuazo <handroissuazo@gmail.com>
2015-12-03 15:33:00 -0600

    Redesign of map

--- CSCE444/SemanticsVisualization/script/sem_vis.js
+++ CSCE444/SemanticsVisualization/script/sem_vis.js
@@ -3,7 +3,9 @@
  */
 
 var _data_overviewCounts = {};
+var dynamicCounts = {};
 var _data_assets_D = [];
+var svg;
 
 $( document ).ready(function() {
     // Init of Globals
@@ -14,11 +16,24 @@ $( document ).ready(function() {
 // DOM Management
 function dom_init() {
     dom_bindLinkButtons();
+    dom_createAndBindDatePicker();
 }
 
 function dom_bindLinkButtons(){
     $('body').on('click', '.linkButton', function(event){
         event_onLinkClicked(event);
+    });
+}
+
+function dom_createAndBindDatePicker() {
+    $("#datepicker").datepicker({
+        onSelect: function(dateText) {
+            updateCountsByDate(dateText);
+        },
+        dateFormat: 'yy-mm-dd'
+    });
+    $("#datepicker").on('change', function(event) {
+
     });
 }
 
@@ -31,49 +46,75 @@ function dom_showLoadingIndicator(){
 }
 
 function dom_CreateMap(){
-    //basic map config with custom fills, mercator projection
-    var map = new Datamap({
 
-        element: document.getElementById('container'),
-        scope: 'usa',
-        height: 1000,
-        fills: {
-            defaultFill: '#f0af0a',
-            lt50: '#00FF66',
-            gt50: 'red'
-        },
-        data: _data_overviewCounts,
-        geographyConfig: {
-            highlightBorderColor: '#FFF',
-            highlightBorderWidth: 3,
-            highlightFillColor: 'rgba(0,200,200,1)',
-            popupTemplate: function(geo, data) {
-                if (data && data.D)
-                    return "<div class='hoverinfo'>" + geo.properties.name + "</br>" + "Total Military Grade Assets: " + data.D + "</div>";
-                else
-                    return "<div class='hoverinfo'>" + geo.properties.name + "</div>";
-            }
-        }
+    var width = 960,
+        height = 500;
+
+    var projection = d3.geo.albersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    d3.json("data/states_usa.topo.json", function(error, us) {
+        if (error) throw error;
+
+        //svg.insert("path", ".graticule")
+        //    .datum(topojson.feature(us, us.objects.land))
+        //    .attr("class", "land")
+        //    .attr("d", path);
+
+        svg.selectAll("path")
+            .data(topojson.feature(us, us.objects.states).features)
+            .enter().append("path")
+            .attr("d", path)
+            .style("fill", function(d) {
+                var value = dynamicCounts[d.properties.name].D;
+                return "rgba(173, 15, 15, " + value / 10000 + ")";
+            })
+            .style("stroke", function(d) {
+                return "rgba(170, 170, 144, 0.27)";
+            })
+            .style("stroke-width", function(d) {
+                return "2px";
+            })
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(1)
+                    .style("opacity", .9);
+                div .html(d.properties.name + "<br/>"  + dynamicCounts[d.properties.name].D)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+        //svg.insert("path", ".graticule")
+        //    .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
+        //    .attr("class", "county-boundary")
+        //    .attr("d", path);
+
+        svg.insert("path", ".graticule")
+            .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+            .attr("class", "state-boundary")
+            .attr("d", path);
 
     });
 
-    //map.bubbles([
-    //    {name: 'Military', latitude: 30.60, longitude: -96.31, radius: 28, fillKey: 'gt50', militarySpending: '36,000', idtag: 'mil' },
-    //    {name: 'Tweets', latitude: 30.60, longitude: -96.31, radius: 18, fillKey: 'lt50', tweets: 2500, idtag: 'tweet'}
-    //], {
-    //    popupTemplate: function(geo, data) {
-    //        if (data.idtag === 'tweet')
-    //            return "<div class='hoverinfo'> " + data.name
-    //                + ' Count on </br> October weekend: ' + data.tweets + "</div>";
-    //        else if (data.idtag === 'mil')
-    //            return "<div class='hoverinfo'> " + data.name
-    //                + ' Spending </br> in 2014: $' + data.militarySpending  + "</div>";
-    //    },
-    //    borderColor: '#000',
-    //    highlightFillColor: '#CC14A6',
-    //    highlightBorderColor: '#FFFFFF',
-    //    highlightBorderWidth: 4
-    //});
+    d3.select(self.frameElement).style("height", height + "px");
+
 }
 
 // Comminucations Management
@@ -88,6 +129,7 @@ function comm_getLesoOverviewCounts(){
         beforeSend: dom_showLoadingIndicator,
         success: function(data) {
             _data_overviewCounts = data;
+            dynamicCounts = jQuery.extend(true, {}, _data_overviewCounts);
         },
         complete: function() {
             dom_hideLoadingIndicator();
@@ -97,6 +139,29 @@ function comm_getLesoOverviewCounts(){
             // Todo: add some error handling to show the user something went wrong.
         }
     });
+}
+
+function updateCountsByDate(date){
+    //Date must be in the format = "2011-11-04"
+    dynamicCounts = jQuery.extend(true, {}, _data_overviewCounts);
+    var d1 = Date.parse(date);
+    for (var i = 0; i < _data_assets_D.length; ++i){
+        var d2 = Date.parse(_data_assets_D[i]["Ship Date"]);
+        if (d1 < d2) {
+            dynamicCounts[_data_assets_D[i].State].D--;
+        }
+    }
+
+    svg.selectAll("path")
+        .style("fill", function(d) {
+            if (d.type == "Feature") {
+                var value = dynamicCounts[d.properties.name].D;
+                return "rgba(173, 15, 15, " + value / dynamicCounts["CA"].D + ")";
+            }
+        });
+
+
+    return dynamicCounts;
 }
 
 function comm_GetLesoD(){
