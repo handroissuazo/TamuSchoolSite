3e9678f73d5441e72fcdef24486d2a27bf0711b5
handroissuazo <alejandrojsuazo@tamu.edu>
2015-12-08 00:03:11 -0600

    Next iteration of the code

--- CSCE444/SemanticsVisualization/script/sem_vis.js
+++ CSCE444/SemanticsVisualization/script/sem_vis.js
@@ -20,26 +20,80 @@ $( document ).ready(function() {
 
 // DOM Management
 function dom_init() {
-    dom_bindLinkButtons();
+    dom_bindButtons();
     dom_createAndBindDatePicker();
 }
 
-function dom_bindLinkButtons(){
-    $('body').on('click', '.linkButton', function(event){
-        event_onLinkClicked(event);
-    });
+function dom_showOverviewButton(){
+    $("#ToOverview").fadeIn();
+}
+
+function dom_hideOverviewButton(){
+    $("#ToOverview").fadeOut();
+}
+
+function dom_bindButtons(){
+    //$('body').on('click', '.linkButton', function(event){
+    //    event_onLinkClicked(event);
+    //});
+    $("#ToOverview").on('click', event_backToOverview);
+}
+
+function dom_createOverviewGraph(){
+    var dateData = makeCountsObjectForOverviewGraph("1998-02-28", "year", "2015-06-01");
+
+    for(var i = 0; i < dateData.length; ++i){
+        var totalDcounts = 0;
+        for (var state in dateData[i].Counts){
+            totalDcounts += dateData[i].Counts[state].D;
+        }
+        dateData[i].totalCount = totalDcounts;
+    }
+
+    makeGraph(dateData, "Increase in military assets in the US since 1998");
+}
+
+function dom_createDateFilteredUSGraph(startDate, endDate, interval) {
+    var dateData = makeCountsObjectForOverviewGraph(startDate, interval, endDate);
+
+    for(var i = 0; i < dateData.length; ++i){
+        var totalDcounts = 0;
+        for (var state in dateData[i].Counts){
+            totalDcounts += dateData[i].Counts[state].D;
+        }
+        dateData[i].totalCount = totalDcounts;
+    }
+
+    makeGraph(dateData, "Increase in military assets in the US from "+ startDate + " to " + endDate);
+}
+
+function dom_createDateFilteredStateGraph(state){
+    var startDate = "1998-02-28";
+    var endDate = $("#datepicker").datepicker("getDate");
+    var interval = "year";
+
+    var dateData = makeCountsObjectForStateGraph(startDate, interval, endDate, state);
+
+    for(var i = 0; i < dateData.length; ++i){
+        var totalDcounts = 0;
+        for (var state in dateData[i].Counts){
+            totalDcounts += dateData[i].Counts[state].D;
+        }
+        dateData[i].totalCount = totalDcounts;
+    }
+
+    makeGraph(dateData, "Increase in military assets in "+ state +" from "+ startDate + " to " + endDate);
 }
 
 function dom_createAndBindDatePicker() {
     $("#datepicker").datepicker({
-        onSelect: function(dateText) {
-            updateCountsByDate(dateText);
+        onSelect: function (dateText) {
+            dynamicCounts = updateCountsByDate(Date.parse(dateText));
+            dom_createDateFilteredUSGraph("1998-02-28", dateText, "year");
         },
         dateFormat: 'yy-mm-dd'
     });
-    $("#datepicker").on('change', function(event) {
-
-    });
+    $("#datepicker").datepicker('setDate', new Date());
 }
 
 function dom_hideLoadingIndicator(){
@@ -104,7 +158,7 @@ function dom_CreateMap(){
                     .style("opacity", 0);
             })
             .on("click", function(d) {
-                state_clicked(d);
+                event_stateClicked(d);
             });
 
         //svg.insert("path", ".graticule")
@@ -115,6 +169,7 @@ function dom_CreateMap(){
     });
 
     d3.select(self.frameElement).style("height", height + "px");
+
 }
 
 function dom_createCounties(state_name){
@@ -129,14 +184,13 @@ function dom_createCounties(state_name){
             .attr("d", path)
             .attr("class", "county")
             .style("fill", function(d) {
-
-                return "rgba(0, 0, 0, 0.0)";
+                return "none";
             })
             .style("stroke", function(d) {
                 return "rgba(170, 170, 144, 0.27)";
             })
             .style("stroke-width", function(d) {
-                return "2px";
+                return "1px";
             })
             .on("mouseover", function(d) {
                 d;
@@ -175,25 +229,101 @@ function comm_getLesoOverviewCounts(){
 
 function updateCountsByDate(date){
     //Date must be in the format = "2011-11-04"
-    dynamicCounts = jQuery.extend(true, {}, _data_overviewCounts);
-    var d1 = Date.parse(date);
+    var newCounts = jQuery.extend(true, {}, _data_overviewCounts);
+    var d1 = date;
     for (var i = 0; i < _data_assets_D.length; ++i){
         var d2 = Date.parse(_data_assets_D[i]["Ship Date"]);
         if (d1 < d2) {
-            dynamicCounts[_data_assets_D[i].State].D--;
+            newCounts[_data_assets_D[i].State].D--;
         }
     }
 
     svg.selectAll("path")
         .style("fill", function(d) {
             if (d.type == "Feature") {
-                var value = dynamicCounts[d.properties.name].D;
-                return "rgba(173, 15, 15, " + value / dynamicCounts["CA"].D + ")";
+                var value = newCounts[d.properties.name].D;
+                return "rgba(173, 15, 15, " + value / newCounts["CA"].D + ")";
             }
         });
 
 
-    return dynamicCounts;
+    return newCounts;
+}
+
+function makeCountsObjectForOverviewGraph(startDate, interval, endDate){
+    var dateData = [];
+    var d1 = Date.parse(startDate);
+    var d2 = Date.parse(endDate);
+    var intervalInSeconds = 31556926000; //year by default
+
+    switch(interval){
+        case "day":
+            intervalInSeconds = 86400000;
+            break;
+        case "week":
+            intervalInSeconds = 604800000;
+            break;
+        case "month":
+            intervalInSeconds = 2629743000;
+            break;
+        //case year will keep the value the same.
+    }
+
+    for (var currentDate = d1; currentDate <= d2; currentDate += intervalInSeconds ) {
+        var countSnapshot = updateCountsByDate(currentDate);
+        var graphObj = {};
+        graphObj.Date = currentDate;
+        graphObj.Counts = countSnapshot;
+        dateData.push(graphObj);
+    }
+
+    return dateData;
+}
+
+function makeCountsObjectForStateGraph(startDate, interval, endDate, state){
+    var dateData = [];
+    var d1 = Date.parse(startDate);
+    var d2 = Date.parse(endDate);
+    var intervalInSeconds = 31556926000; //year by default
+
+    switch(interval){
+        case "day":
+            intervalInSeconds = 86400000;
+            break;
+        case "week":
+            intervalInSeconds = 604800000;
+            break;
+        case "month":
+            intervalInSeconds = 2629743000;
+            break;
+        //case year will keep the value the same.
+    }
+
+    for (var currentDate = d1; currentDate <= d2; currentDate += intervalInSeconds ) {
+        var countSnapshot = updateCountsByDateRangeAndState(startDate, currentDate, state);
+        var graphObj = {};
+        graphObj.Date = currentDate;
+        graphObj.Counts = countSnapshot;
+        dateData.push(graphObj);
+    }
+
+    return dateData;
+}
+
+function updateCountsByDateRangeAndState(startDate, currentDate, state){
+    //Date must be in the format = "2011-11-04"
+    var newCounts = {};
+    newCounts[state] = {};
+    newCounts[state].D = 0;
+    var d1 = Date.parse(startDate);
+    for (var i = 0; i < _data_assets_D.length; ++i){
+        var d2 = Date.parse(_data_assets_D[i]["Ship Date"]);
+        if (d1 < d2 <= currentDate && _data_assets_D[i].State == state) {
+            newCounts[state].D++;
+        }
+    }
+
+    return newCounts;
 }
 
 function comm_GetLesoD(){
@@ -202,6 +332,7 @@ function comm_GetLesoD(){
         beforeSend: function() {},
         success: function(data) {
             _data_assets_D = data;
+            dom_createOverviewGraph();
         },
         complete: function() {
             // Todo: add some post processing on the information.
@@ -219,29 +350,45 @@ function event_onLinkClicked(e){
     window.open(url, '_blank');
 }
 
-function clear_set_state_fill(state){
-    svg.selectAll("path")
-        .style("fill", function(d) {
-            if(d.id == state.id) return "#FFFFFF";
+function dom_clear_set_state_fill(state){
+    g.selectAll("path").transition()
+        .duration(450)
+        .style("fill", function (d) {
+            if (d.id == state.id) return "#FFFFFF";
             else return "#CCCCCC"
         });
 }
 
-function state_clicked(d) {
+function dom_set_overview_state_fill(){
+    g.selectAll("path").transition()
+        .duration(450)
+        .style("fill", function(d) {
+            var value = dynamicCounts[d.properties.name].D;
+            return "rgba(173, 15, 15, " + value / 10000 + ")";
+        })
+}
+
+function event_stateClicked(d) {
     var state = null
 
     if (d && state !== d) {
-        var xyz = get_xyz(d);
+        //var xyz = get_xyz(d);
         state = d;
-        var state_name = state.properties.name;
-        dom_createCounties(state_name);
-        clear_set_state_fill(state);
-        zoom(xyz);
+        dom_clear_set_state_fill(state);
+        //zoom(xyz);
+        setTimeout(function(d){
+            dom_createDateFilteredStateGraph(state.properties.name);}
+            , 1000);
+        dom_showOverviewButton();
     } else {
         state = null;
     }
 }
 
+function event_backToOverview(){
+    dom_hideOverviewButton();
+    dom_set_overview_state_fill();
+}
 
 //Helper Functions
 function zoom(xyz) {
@@ -264,57 +411,194 @@ function get_xyz(d) {
     return [x, y, z];
 }
 
-function string_similarity(sa1, sa2){
-    // Compare two strings to see how similar they are.
-    // Answer is returned as a value from 0 - 1
-    // 1 indicates a perfect similarity (100%) while 0 indicates no similarity (0%)
-    // Algorithm is set up to closely mimic the mathematical formula from
-    // the article describing the algorithm, for clarity.
-    // Algorithm source site: http://www.catalysoft.com/articles/StrikeAMatch.html
-    // (Most specifically the slightly cryptic variable names were written as such
-    // to mirror the mathematical implementation on the source site)
-    //
-    // 2014-04-03
-    // Found out that the algorithm is an implementation of the Sørensen–Dice coefficient [1]
-    // [1] http://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
-    //
-    // The algorithm is an n-gram comparison of bigrams of characters in a string
+// Graph scripts
+function makeGraph(dateData, graphTitle){
+    $("#GraphSpace").empty();
+    $("#GraphTitle").empty();
+    $("#GraphTitle").html(graphTitle);
+
+    // Set the dimensions of the canvas / graph
+    var margin = {top: 30, right: 20, bottom: 30, left: 70},
+        width = $('#InformationSideBar').width() - margin.left - margin.right,
+        height = 270 - margin.top - margin.bottom;
+
+// Parse the date / time
+    var parseDate = d3.time.format("%d-%b-%y").parse,
+        formatDate = d3.time.format("%d-%b"),
+        bisectDate = d3.bisector(function(d) { return d.date; }).left;
+
+// Set the ranges
+    var x = d3.time.scale().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+// Define the axes
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(5);
+
+    var yAxis = d3.svg.axis().scale(y)
+        .orient("left").ticks(5);
+
+// Define the line
+    var valueline = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.close); });
+
+// Adds the svg canvas
+    var svgGraph = d3.select("#GraphSpace")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    var lineSvg = svgGraph.append("g");
+
+    var focus = svgGraph.append("g")
+        .style("display", "none");
+
+// Get the data
+    var data = [];
+    dateData.forEach(function (d) {
+        var newObj = {};
+        newObj.date = d.Date;
+        newObj.close = +d.totalCount;
+        data.push(newObj);
+    });
+
+    // Scale the range of the data
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+        // Add the valueline path.
+        lineSvg.append("path")
+            .attr("class", "line")
+            .attr("d", valueline(data))
+            .style("fill", "none")
+            .style("stroke-width", 3)
+            .style("stroke", "blue");
 
 
-    // for my purposes, comparison should not check case or whitespace
-    var s1 = sa1.replace(/\s/g, "").toLowerCase();
-    var s2 = sa2.replace(/\s/g, "").toLowerCase();
+        // Add the X Axis
+        svgGraph.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
-    function intersect(arr1, arr2) {
-        // I didn't write this.  I'd like to come back sometime
-        // and write my own intersection algorithm.  This one seems
-        // clean and fast, though.  Going to try to find out where
-        // I got it for attribution.  Not sure right now.
-        var r = [], o = {}, l = arr2.length, i, v;
-        for (i = 0; i < l; i++) {
-            o[arr2[i]] = true;
+        // Add the Y Axis
+        svgGraph.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        // append the x line
+        focus.append("line")
+            .attr("class", "x")
+            .style("stroke", "blue")
+            .style("stroke-dasharray", "3,3")
+            .style("opacity", 0.5)
+            .attr("y1", 0)
+            .attr("y2", height);
+
+        // append the y line
+        focus.append("line")
+            .attr("class", "y")
+            .style("stroke", "blue")
+            .style("stroke-dasharray", "3,3")
+            .style("opacity", 0.5)
+            .attr("x1", width)
+            .attr("x2", width);
+
+        // append the circle at the intersection
+        focus.append("circle")
+            .attr("class", "y")
+            .style("fill", "none")
+            .style("stroke", "blue")
+            .attr("r", 4);
+
+        // place the value at the intersection
+        focus.append("text")
+            .attr("class", "y1")
+            .style("stroke", "white")
+            .style("stroke-width", "3.5px")
+            .style("opacity", 0.8)
+            .attr("dx", 8)
+            .attr("dy", "-.3em");
+        focus.append("text")
+            .attr("class", "y2")
+            .attr("dx", 8)
+            .attr("dy", "-.3em");
+
+        // place the date at the intersection
+        focus.append("text")
+            .attr("class", "y3")
+            .style("stroke", "white")
+            .style("stroke-width", "3.5px")
+            .style("opacity", 0.8)
+            .attr("dx", 8)
+            .attr("dy", "1em");
+        focus.append("text")
+            .attr("class", "y4")
+            .attr("dx", 8)
+            .attr("dy", "1em");
+
+        // append the rectangle to capture mouse
+        svgGraph.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .on("mouseover", function() { focus.style("display", null); })
+            .on("mouseout", function() { focus.style("display", "none"); })
+            .on("mousemove", mousemove);
+
+        function mousemove() {
+            var x0 = x.invert(d3.mouse(this)[0]),
+                i = bisectDate(data, x0, 1),
+                d0 = data[i - 1],
+                d1 = data[i],
+                d = x0 - d0.date > d1.date - x0.getTime() ? d1 : d0;
+
+            focus.select("circle.y")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                    y(d.close) + ")");
+
+            focus.select("text.y1")
+                .attr("transform",
+                    "translate(" + x(d.date - d.date/10) + "," +
+                    y(d.close) + ")")
+                .text(d.close);
+
+            focus.select("text.y2")
+                .attr("transform",
+                    "translate(" + x(d.date - d.date/10) + "," +
+                    y(d.close) + ")")
+                .text(d.close);
+
+            var theDate = new Date(d.date);
+            focus.select("text.y3")
+                .attr("transform",
+                    "translate(" + x(d.date - d.date/10) + "," +
+                    y(d.close) + ")")
+                .text(theDate.toLocaleDateString('en-US', {'year': 'numeric'}));
+
+            focus.select("text.y4")
+                .attr("transform",
+                    "translate(" + x(d.date - d.date/10) + "," +
+                    y(d.close) + ")")
+                .text(theDate.toLocaleDateString('en-US', {'year': 'numeric'}));
+
+            focus.select(".x")
+                .attr("transform",
+                    "translate(" + x(d.date) + "," +
+                    y(d.close) + ")")
+                .attr("y2", height - y(d.close));
+
+            focus.select(".y")
+                .attr("transform",
+                    "translate(" + width * -1 + "," +
+                    y(d.close) + ")")
+                .attr("x2", width + width);
         }
-        l = arr1.length;
-        for (i = 0; i < l; i++) {
-            v = arr1[i];
-            if (v in o) {
-                r.push(v);
-            }
-        }
-        return r;
-    }
 
-    var pairs = function(s){
-        // Get an array of all pairs of adjacent letters in a string
-        var pairs = [];
-        for(var i = 0; i < s.length - 1; i++){
-            pairs[i] = s.slice(i, i+2);
-        }
-        return pairs;
-    }
-
-    var similarity_num = 2 * intersect(pairs(s1), pairs(s2)).length;
-    var similarity_den = pairs(s1).length + pairs(s2).length;
-    var similarity = similarity_num / similarity_den;
-    return similarity;
-};
+}
